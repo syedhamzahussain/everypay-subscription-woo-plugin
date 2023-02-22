95e648724afaa399abd0d3ebc11cf6826d6f5548
Syed Hamza Hussain <hamza.hussain335@gmail.com>
2023-02-22 07:25:48 +0500

    add order reference for all type of orders

--- includes/class-eppg-wc.php
+++ includes/class-eppg-wc.php
@@ -131,10 +131,12 @@ if ( ! class_exists( 'EPGG_WC' ) ) {
 			   // LOG THE FAILED ORDER TO CUSTOM "failed-orders" LOG
 			   $logger->info( wc_print_r( $order_created, true ), array( 'source' => 'first-payments' ) );
 
+			   update_post_meta( $order_id, 'order_reference', $order_created->order_reference );
+
 				if (WC_Subscriptions_Order::order_contains_subscription($order_id)) {
-					update_post_meta( $order_id, 'order_reference', $order_created->order_reference );
 					WC_Subscriptions_Manager::activate_subscriptions_for_order($customer_order);
 				}
+				
 				return array(
 					'result'   => 'success',
 					'redirect' => $order_created->payment_link,
